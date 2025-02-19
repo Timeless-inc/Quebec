39808193b03b080ec21bc43a40b1f680d294294a
Andersugm <andersuemp@gmail.com>
2025-02-19 11:19:06 -0300

    botão cadastrar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,6 +60,9 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
+            <a style="margin-right: 15%; color:green;" class="underline text-sm link-success rounded-md " href="{{ route('register') }}">
+                {{ __('Cadastrar-se') }}
+            </a>
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Esqueceu sua senha?') }}
                 </a>
