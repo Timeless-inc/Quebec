eeed2b76a7405dcb221cdf0b362ae490d6dddaae
Brenno Victor <brennovictor22@gmail.com>
2025-02-23 22:53:03 -0300

    Alterações do design da tela inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SRE | Timeless</title>
 
-    <!-- Bootstrap CSS -->
+    <link rel = "stylesheet" type="text/css" href="/css/style.css"
+ 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <style>
+ 
+   <!-- <style>
         body {
             font-family: 'Figtree', sans-serif;
             background-color: #dce3d5; /* Cor do fundo da seção principal */
@@ -59,41 +59,40 @@
         .hero-image img {
             max-width: 400px;
         }
-    </style>
+    </style> -->
 </head>
 
 <body>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="#">SRE.</a>
-        <div class="ms-auto">
-            <a href="{{ url('/dashboard') }}" class="text-dark me-3">Tutorial</a>
-            <a href="{{ url('/dashboard') }}" class="text-dark me-3">Equipe</a>
-            <a href="{{ url('/dashboard') }}" class="btn btn-success">Login</a>
+    <header class="cabecalho">
+        <a class="cabecalho-logo" href="#">SRE.</a>
+        <nav class="cabecalho-menu">
+        <a class="cabecalho-menu-item" href="{{ url('/dashboard') }}">Início</a>
+            <a class="cabecalho-menu-item" href="{{ url('/dashboard') }}">Tutorial</a>
+            <a class="cabecalho-menu-item" href="{{ url('/dashboard') }}">Equipe</a>
+            <a class="cabecalho-menu-item" href="{{ url('/dashboard') }}">Acessar</a>
+            <a class="cabecalho-menu-item" href="{{ url('/dashboard') }}">Cadastrar</a>       
+        </nav>
+    </header>
 
+    <main class="conteudo">
+        <section class="conteudo-principal">
+            <div class="conteudo-principal-escrito">
+                <h1 class="conteudo-principal-escrito-titulo">Sistema de <br>Requerimento <br><span class="conteudo-principal-escrito-titulo-estudantil">Estudantil</span></h1>
+                <p class="conteudo-principal-escrito-subtitulo">Acesse agora um ambiente simples e intuitivo <br> para gerenciar suas demandas estudantis!</p>
+                <div class="botoes-container"> 
+                    <button class="conteudo-principal-escrito-botao-informacao">Saiba Mais</button>
+                    <a href="{{ url('/dashboard') }}"><button class="conteudo-principal-escrito-botao-acesso">Acessar Sistema</button></a>
+                </div>
+            </div>
+            <img class="conteudo-principal-imagem" src="/svg/ilustracao.svg" alt="Ilustração do Sistema">
+        </section>
+    </main>
+
+    <footer>
         
-        </div>
-    </nav>
-
-    <!-- Hero Section -->
-    <div class="hero-section">
-        <!-- Texto -->
-        <div class="hero-text">
-            <h1>Sistema de requerimento do estudante.</h1>
-            <p>O SRE foi criado para tornar suas solicitações acadêmicas mais rápidas e eficientes. Envie pedidos, acompanhe o status e resolva
-                suas pendências sem burocracia, tudo em um só lugar<br>
-                Cadastre-se agora e tenha acesso a um ambiente simples e intuitivo para gerenciar suas demandas estudantis!
-            </p>
-            <a href="{{ route('register') }}" class="btn btn-success ms-2">Cadastre-se</a>
-        </div>
-
-        <!-- Imagem -->
-        <div class="hero-image">
-            <img src="/img/estudante.png" alt="Estudante no computador">
-        </div>
-    </div>
-
+    </footer>
+   
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
